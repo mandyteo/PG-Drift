44d050e617b08cb5aa2e6717579cf519351be4c3
mandyteo <mandy.teo.hx@protonmail.com>
2025-12-22 11:51:00 +0800

    Add target database

--- src/config/pg_config.py
+++ src/config/pg_config.py
@@ -4,11 +4,12 @@ from typing import List
 import os
 
 class PgConfig:
-    def __init__(self, host: str, port: int, user: str, password: str):
+    def __init__(self, host: str, port: int, user: str, password: str, database: str):
         self.host = host
         self.port = port
         self.user = user
         self.password = password
+        self.database = database
 
     @staticmethod
     def load_from_env() -> List[PgConfig]:
@@ -20,7 +21,8 @@ class PgConfig:
             port = int(os.getenv(f"PG_DRIFT_DB_PORT_{i}", "5432"))
             user = os.getenv(f"PG_DRIFT_DB_USER_{i}", "postgres")
             password = os.getenv(f"PG_DRIFT_DB_PASSWORD_{i}", "password")
+            database = os.getenv(f"PG_DRIFT_DB_NAME_{i}", "postgres")
 
-            config = PgConfig(host, port, user, password)
+            config = PgConfig(host, port, user, password, database)
             configs.append(config)
         return configs
